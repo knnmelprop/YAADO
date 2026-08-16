2b2638aca052250d33affe0c8c6803f5f0fa75d3
Arseni10Lk <arseniy230606@gmail.com>
2026-08-16 11:52:39 +0200

    refactor(initial-architecture): low-level architecture setup
    Signed-off-by: Arseni10Lk <arseniy230606@gmail.com>

--- IADE_Core/modules/aerodynamics/__init__.py
+++
@@ -1,6 +0,0 @@
-# MELprop-IADE | analyses.aerodynamics | v0.1.0
-"""Aerodynamic analyses for MELprop-IADE vehicles."""
-
-from IADE_Core.modules.aerodynamics.avl_wrapper import AVLAnalysis
-
-__all__ = ["AVLAnalysis"]
