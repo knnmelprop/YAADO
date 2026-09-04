461b85ead6eb77bf3555af176f8e3bcf1dd3c7c9
Arseni10Lk <arseniy230606@gmail.com>
2026-09-05 01:20:34 +0200

    feat(template-generator): outline toml generation logic
    Signed-off-by: Arseni10Lk <arseniy230606@gmail.com>

--- Terminal/Assembly/template_generator.py
+++ Terminal/Assembly/template_generator.py
@@ -1,8 +1,15 @@
+import os
+
 from pydantic import BaseModel
 
 from YAADO_Core import ComponentStore
 from YAADO_Core.Foundation import BaseVehicleConfig
 
+def generate_template(name: str, components: list[BaseModel]) -> None:
+    template_vehicle = assemble_vehicle(name, components)
+
+    os.makedirs(f"Hangar/{name}/", exist_ok=True)
+    template_vehicle.to_toml(f"Hangar/{name}/{name}.toml")
 
 def assemble_vehicle(name: str, components: list[BaseModel]) -> BaseVehicleConfig:
     template_vehicle = BaseVehicleConfig(name=name)
